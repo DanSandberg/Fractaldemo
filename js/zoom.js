1b4b6acd3e7175c2c08832810fd5a629f26bdd04
danneelitz <danneelitz@gmail.com>
2018-01-13 17:49:59 +0100

    zoom tillagd
    Zoomen tillagd och funkar

---
+++ js/zoom.js
@@ -0,0 +1,31 @@
+$("#image1").imgViewer2({
+
+  // zoom step
+
+  zoomStep: 0.5,
+
+
+
+  // the limit on the maximum zoom level of the image
+
+  zoomMax: undefined,
+
+
+
+  // is zoomable
+
+  zoomable: true,
+
+
+
+  // is draggable
+
+  dragable: true,
+
+  // callbacks
+
+  onClick: $.noop,
+
+  onReady: $.noop
+
+});
